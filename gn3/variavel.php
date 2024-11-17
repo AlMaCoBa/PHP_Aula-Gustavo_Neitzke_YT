f57c81bc7a715941bb8ebabe8d7668445017898e
Alberto Marques <albertomcbatista@outlook.com>
2024-11-16 21:51:51 -0300

    comentario

---
+++ gn3/variavel.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>#3</title>
+</head>
+<body>
+
+    <?php 
+    
+        $gustavo = 17;
+        $giulia = "a giulia tem 22 anos";
+        echo $giulia ." e o gustavo tem " .$gustavo . "<br><br>";
+        $x = 5;
+        $y = 2;
+        echo  $x + $y;
+    ?>
+    
+</body>
+</html>
